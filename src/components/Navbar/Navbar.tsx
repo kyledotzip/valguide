a7dce6f7e5904032ec44a0710ef2e2d5c2fea318
BuildTools <unconfigured@null.spigotmc.org>
2023-01-17 07:19:49 -0700

    2

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../../assets/valorant.png'
+
+const Navbar = () => {
+    
+    return (
+        <div className = 'val__navbar'>
+            <div className = 'val__navbar-links'>
+                <div className = 'val__navbar-links-logo'>
+                    <img src={logo} alt = "logo" />
+                </div>
+                <div className = 'val__navbar-links-container'>
+                    <p className=".val__navbar-home"><Link to = "home">HOME</Link></p>
+                    <p className=".val__navbar-agents"><Link to = "agents">AGENTS</Link></p>
+                    <p className=".val__navbar-lineups"><Link to = "lineups">LINEUPS</Link></p>
+                    <p className=".val__navbar-info"><Link to = "info">INFO</Link></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/Pages/Info.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './Info.css';
+
+const Info = () => {
+    return (
+        <div className="info-page">
+            <div className="info-banner">
+                <h1>INFO</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Info;
